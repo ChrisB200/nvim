070a50c0758ebda21731d0d45cb4f6237455a538
Christopher Bonner <cbonner.dev@outlook.com>
2024-02-19 17:57:16 +0000

    Live server Plugin

--- lua/chris/packer.lua
+++ lua/chris/packer.lua
@@ -2,6 +2,9 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    -- Live Server Plugin
+    use 'barrett-ruth/live-server.nvim'
+
     -- Command Auto complete
     use 'gelguy/wilder.nvim'
 
--- lua/chris/remap.lua
+++ lua/chris/remap.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', {noremap = true, silent = 
 
 -- Can move lines up and down the file
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Switch Projects
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")

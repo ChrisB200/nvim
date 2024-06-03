dc8729c33e541e8ca7fb9e51dbfee401bcdd87e9
Christopher Bonner <cbonner.dev@outlook.com>
2024-06-03 17:11:44 +0100

    some change - i forgot

--- lua/chris/init.lua
+++ lua/chris/init.lua
@@ -1,6 +1,2 @@
 require("chris.remap")
 require("chris.set")
-vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber')
--- Automatic formatting on save
-vim.cmd([[autocmd BufWritePre *.py execute ':!black %']])
-
